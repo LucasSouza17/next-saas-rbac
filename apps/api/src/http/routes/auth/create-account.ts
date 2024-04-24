6f0a9e9c179765bb1078b26c5b3d35ab7b68686c
Lucas Souza <lucasnando800@hotmail.com>
2024-04-23 22:31:54 -0300

    Create database & Create user route & swagger integration

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,48 @@
+import { hash } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/users',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Create a new user',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { email, name, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: 'user with same email already exists.' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
+    },
+  )
+}
---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,41 @@
+import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { createAccount } from './routes/auth/create-account'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS RBAC',
+      description: 'Full-stack SaaS with multi-tenant & RBAC.',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyCors)
+
+app.register(createAccount)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running on http://localhost:3333')
+})
